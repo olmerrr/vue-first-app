dc9aedaf330a069b5c74c03a023d822a26ab6835
Yaroslav <dombrovskiy91@gmail.com>
2020-11-23 16:51:57 +0200

    v-fore + in components

--- src/App.vue
+++ src/App.vue
@@ -8,21 +8,24 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
-    
+    <course-goals></course-goals>
+
   </div>
 </template>
 
 
 <script>
 import TheHeader from './components/TheHeader.vue';
-import BadgeList from './components/BadgeList';
-import UserInfo from './components/UserInfo';
+import BadgeList from './components/BadgeList.vue';
+import UserInfo from './components/UserInfo.vue';
+import CourseGoals from './components/CourseGoals.vue';
 
 export default {
   components: {
      TheHeader,
      BadgeList,
      UserInfo,
+     CourseGoals
   },
   data() {
       return {
--- src/components/CourseGoals.vue
+++ src/components/CourseGoals.vue
@@ -1,17 +1,21 @@
 <template>
   <ul>
     <li v-for="goal in goals" :key="goal">
-      <slot :item="goal" another-prop="..."></slot>
+      {{ goal }}
     </li>
   </ul>
 </template>
-
 <script>
 export default {
   data() {
     return {
-      goals: ['Finish the course', 'Learn Vue'],
-    };
+      goals: ['Finish the course', 'Lern Vue'],
+    }
   },
-};
+}
 </script>
+<style scoped>
+  ul,li {
+    list-style: none
+  }
+</style>
